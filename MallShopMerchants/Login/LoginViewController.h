1505abc0f3eef9b9a605322f0dfd70e525cdc287
zhangbin <bin.zhang1@husor.com>
2019-05-07 22:18:07 +0800

    login

---
+++ MallShopMerchants/Login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  MallShopMerchants
+//
+//  Created by benzhang on 2019/5/7.
+//  Copyright © 2019年 yezhongzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ MallShopMerchants/Login/View/LoginIconView.m
@@ -0,0 +1,21 @@
+//
+//  LoginIconView.m
+//  MallShopMerchants
+//
+//  Created by benzhang on 2019/5/7.
+//  Copyright © 2019年 yezhongzheng. All rights reserved.
+//
+
+#import "LoginIconView.h"
+
+@implementation LoginIconView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
